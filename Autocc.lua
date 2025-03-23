c26385a9e6ddb4265f6b09424cd62629128227f1
Duc Designer <ducnmth05622@gmail.com>
2025-03-23 22:41:27 +0700

    Update Autocc.lua

--- Autocc.lua
+++ Autocc.lua
@@ -1,1 +1,5 @@
-cc
+if getgenv().Language == "Vietnamese" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ccditmethkskider/Ducv4/refs/heads/main/mainvn"))()
+elseif getgenv().Language == "English" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ccditmethkskider/Ducv4/refs/heads/main/maineg"))()
+end
